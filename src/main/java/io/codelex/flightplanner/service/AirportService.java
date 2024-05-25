cf42658571228a38fd5c71107f7a97f9419e9153
ShadyWestHaze <zakatistovu@gmail.com>
2024-05-25 13:11:28 +0300

    deleted all

--- src/main/java/io/codelex/flightplanner/service/AirportService.java
+++
@@ -1,9 +0,0 @@
-package io.codelex.flightplanner.service;
-
-import io.codelex.flightplanner.model.Airport;
-
-import java.util.List;
-
-public interface AirportService {
-    List<Airport> searchAirports(String search);
-}//comment for pull
